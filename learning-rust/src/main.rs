b3905f3503d4d712e9f48d0fee4896d14348f06e
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2023-03-19 10:11:40 +0100

    base

--- learning-rust/src/main.rs
+++ learning-rust/src/main.rs
@@ -1,13 +1,33 @@
-use std::{thread, time};
+use druid::{Data, widget::{Label, Button, Flex}, Env, Widget, WindowDesc, AppLauncher};
+/*
+ * Data 
+ * Ui Builder
+ * Main
+ */
+
+#[derive(Clone, Data)]
+struct FunnyData {
+    num: i32
+}
+
+fn ui_builder() -> impl Widget<FunnyData> {
+    // Counter: _
+    // + -
+    let label = Label::new(|data: &FunnyData, _: &Env| format!("Counter: {}", data.num));
+    let increment = Button::new("+")
+        .on_click(|_ctx, data: &mut FunnyData, _env| data.num += 1);
+    let decrement = Button::new("-")
+        .on_click(|_ctx, data: &mut FunnyData, _env| data.num -= 1);
+
+    Flex::column().with_child(label).with_child(Flex::row().with_child(increment).with_child(decrement))
+}
 
 fn main() {
-    let line1 , line4 = ["_" ; 5];
-    loop {
-        thread::sleep(time::Duration::from_millis(200));
-        print!("{}[2J", 27 as char);
-        println!(line1);
-        println!(line2);
-        println!(line3);
-        println!(line4);
-    }
+    // Window Descriptor
+    // Launch to the stars
+    let main_window = WindowDesc::new(ui_builder())
+        .title("Funny Window");
+    AppLauncher::with_window(main_window)
+        .log_to_console()
+        .launch(FunnyData { num: 0 }).unwrap();
 }
